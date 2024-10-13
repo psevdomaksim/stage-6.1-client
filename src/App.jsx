5c3e072e53a36da5767e42550a0c53111be1fd33
psevdomaksim <varmax2004@gmail.com>
2024-10-14 00:49:18 +0300

    Add stage 6.1 front functionality

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Link } from "react-router-dom";
+import "./App.css";
+import AppRoutes from "./components/AppRoutes";
+import { ADD_POST_ROUTE, POST_LIST_ROUTE } from "./utils/routes_consts";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="container">
+          <div className="header">
+            <Link to={POST_LIST_ROUTE}>
+              {" "}
+              <h1>Post List</h1>
+            </Link>
+
+            <Link className="add-post-btn" to={ADD_POST_ROUTE}>
+              Add Post
+            </Link>
+          </div>
+          <AppRoutes />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
